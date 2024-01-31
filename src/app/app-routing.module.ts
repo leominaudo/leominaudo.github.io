d5cc9467b1bdb6ed28ed1abd290dcde639f2c21d
Leonardo Minaudo <leonardo.minaudo@studenti.unipr.it>
2024-01-31 16:40:56 +0100

    angular 17

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,16 +1,14 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-import { RouterModule, Routes } from '@angular/router';
+import { Route, RouterModule, Routes } from '@angular/router';
 import { HomeComponent } from './home/home.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: "", component: HomeComponent },
+];
 
 @NgModule({
   imports: [BrowserModule, RouterModule.forRoot(routes)],
   exports: [RouterModule]
 })
-export class AppRoutingModule {
-  routes = [
-    { path: "", component: HomeComponent },
-  ];
- }
+export class AppRoutingModule { }
