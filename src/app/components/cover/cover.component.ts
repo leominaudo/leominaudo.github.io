f7700e9080de183a2e8beec363755de3029bf57d
Leonardo Minaudo <leonardo.minaudo@studenti.unipr.it>
2024-04-26 17:53:24 +0200

    cover

--- src/app/components/cover/cover.component.ts
+++ src/app/components/cover/cover.component.ts
@@ -1,5 +1,21 @@
 import { CommonModule } from '@angular/common';
-import { Component } from '@angular/core';
+import {
+  AfterViewInit,
+  Component,
+  ElementRef,
+  OnDestroy,
+  OnInit,
+  ViewChild,
+  inject,
+} from '@angular/core';
+import {
+  Subscription,
+  debounceTime,
+  distinctUntilChanged,
+  fromEvent,
+  map,
+  startWith,
+} from 'rxjs';
 
 @Component({
   selector: 'app-cover',
@@ -8,11 +24,35 @@ import { Component } from '@angular/core';
   templateUrl: './cover.component.html',
   styleUrl: './cover.component.scss',
 })
-export class CoverComponent {
-  space: number = 30;
+export class CoverComponent implements AfterViewInit, OnDestroy {
+  space: number = 45;
   orange: string = '#ff7514';
   green: string = '#77dd77';
   red: string = '#ec5353';
   grey: string = '#c0c0c0';
   blue: string = '#5d9b9b';
+
+  @ViewChild('innerDiv', { static: true })
+  innerDivRef!: ElementRef;
+  outerDivHeight: number = 275;
+  outerDivHeight$!: Subscription;
+
+  constructor(private elementRef: ElementRef) {}
+
+  ngAfterViewInit(): void {
+    this.outerDivHeight$ = fromEvent(window, 'resize')
+      .pipe(
+        map(() => this.innerDivRef.nativeElement.offsetHeight),
+        distinctUntilChanged()
+      )
+      .subscribe((height) => {
+        this.outerDivHeight = height;
+      });
+
+    this.outerDivHeight = this.innerDivRef.nativeElement.offsetHeight;
+  }
+
+  ngOnDestroy(): void {
+    this.outerDivHeight$?.unsubscribe();
+  }
 }
